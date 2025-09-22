c92cd2bf37bd867482cac77bb781235f68d776ff
Djafta <djafthy@gmail.com>
2025-09-22 10:08:49 +0200

    [Header] Refactor Navbar links and add dropdown for qualifications

--- app/header.tsx
+++ app/header.tsx
@@ -10,9 +10,8 @@ import {
   NavbarMenuToggle,
 } from "@heroui/navbar";
 import { Button } from "@heroui/button";
-import { Link } from "@heroui/link";
 import { useState } from "react";
-import NextLink from "next/link";
+import Link from "next/link";
 import Image from "next/image";
 
 export function Header() {
@@ -28,24 +27,71 @@ export function Header() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden md:flex gap-4" justify="center">
+      <NavbarContent className="hidden md:flex gap-10" justify="center">
         <NavbarItem>
-          <Link as={NextLink} className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/about">
+          <Link className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/about">
             Sobre
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link as={NextLink} className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/licensing">
+          <Link className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/licensing">
             Licenciamento
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link as={NextLink} className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/qualifications">
+        <NavbarItem className={"group h-full items-center flex relative text-gray-600 hover:text-primary"}>
+          <Link className="text-sm font-medium" href="/qualifications">
             Qualificações
           </Link>
+          <aside
+            className={
+              "absolute w-sm top-full invisible opacity-0 translate-y-2 -translate-x-1/2 duration-300 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 bg-background px-2 py-3 border-t-2 border-t-primary text-gray-600 shadow-lg rounded-b"
+            }
+          >
+            <div>
+              <h2 className={"text-md font-bold p-3"}>Áreas de qualificações</h2>
+            </div>
+            <div className={"grid grid-cols-1"}>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/tecnologia-da-informacao"}
+              >
+                Tecnologia da Informação
+              </Link>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/engenharia"}
+              >
+                Engenharia
+              </Link>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/saude"}
+              >
+                Saúde
+              </Link>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/administracao"}
+              >
+                Administração
+              </Link>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/turismo"}
+              >
+                Turismo
+              </Link>
+              <Link
+                className={"text-sm p-3 hover:bg-primary/20 transition-all duration-300 rounded"}
+                href={"/qualifications/agricultura"}
+              >
+                Agricultura
+              </Link>
+            </div>
+          </aside>
         </NavbarItem>
         <NavbarItem>
-          <Link as={NextLink} className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/news">
+          <Link className="text-gray-600 hover:text-gray-900 text-sm font-medium" href="/news">
             Notícias
           </Link>
         </NavbarItem>
@@ -74,27 +120,27 @@ export function Header() {
       </NavbarContent>
       <NavbarMenu>
         <NavbarMenuItem>
-          <Link as={NextLink} className="w-full text-gray-600 hover:text-gray-900" href="/about" size="lg">
+          <Link className="w-full text-gray-600 hover:text-gray-900" href="/about">
             Sobre
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link as={NextLink} className="w-full text-gray-600 hover:text-gray-900" href="/licensing" size="lg">
+          <Link className="w-full text-gray-600 hover:text-gray-900" href="/licensing">
             Licenciamento
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link as={NextLink} className="w-full text-gray-600 hover:text-gray-900" href="/qualifications" size="lg">
+          <Link className="w-full text-gray-600 hover:text-gray-900" href="/qualifications">
             Qualificações
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link as={NextLink} className="w-full text-gray-600 hover:text-gray-900" href="/news" size="lg">
+          <Link className="w-full text-gray-600 hover:text-gray-900" href="/news">
             Notícias
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-          <Link as={NextLink} className="w-full text-gray-600 hover:text-gray-900" href="/sign-in" size="lg">
+          <Link className="w-full text-gray-600 hover:text-gray-900" href="/sign-in">
             Entrar
           </Link>
         </NavbarMenuItem>
