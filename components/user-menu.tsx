0c627fe98016cf698057322b5fbc9bc35abd0c1c
Djafta <djafta@muendane.com>
2026-03-31 12:31:13 +0200

    Enhance user menu with avatar display and improved layout

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -9,9 +9,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
+import { BadgeCheck, LogOut } from "lucide-react";
 
 export type User = {
   email: string
@@ -42,22 +43,41 @@ export function UserMenu({ user }: UserMenuProps) {
 
       <DropdownMenuContent
         align="end"
-        className="w-72 rounded-2xl shadow-xl p-2"
+        className="min-w-64 rounded-2xl shadow-xl"
       >
-        <DropdownMenuLabel className="space-y-1">
-          <p className="text-sm">
-            { user.name }
-          </p>
-          <p className="font-semibold text-xs">
-            { user.email }
-          </p>
+        <DropdownMenuLabel className="p-0 font-normal">
+          <div className="flex items-center gap-2 px-3 py-2">
+            <Avatar className="h-8 w-8 rounded-full">
+              <AvatarImage src={ '' } alt={ user.name }/>
+              <AvatarFallback className="rounded-full bg-primary text-white">
+                { initials }
+              </AvatarFallback>
+            </Avatar>
+
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-medium">{ user.name }</span>
+              <span className="truncate text-xs text-muted-foreground">
+                    { user.email }
+                  </span>
+            </div>
+          </div>
         </DropdownMenuLabel>
+
+        <DropdownMenuSeparator/>
+
+        <DropdownMenuItem asChild>
+          <DropdownMenuItem>
+            <BadgeCheck className="mr-2 h-4 w-4"/>
+            Conta
+          </DropdownMenuItem>
+        </DropdownMenuItem>
 
         <DropdownMenuSeparator/>
 
         <DropdownMenuItem asChild className="text-red-600">
           <Link href={ "/sign-out" }>
-            Terminar Sessão
+            <LogOut className="mr-2 h-4 w-4"/>
+            Sair
           </Link>
         </DropdownMenuItem>
       </DropdownMenuContent>
