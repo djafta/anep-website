4f5c0c68193b6baa791c06206dc2de16d310ad4a
Djafta <djafta@muendane.com>
2026-04-01 12:14:16 +0200

    Enhance icon display in user account card for improved layout

--- app/admin/(admin)/fields/page.tsx
+++ app/admin/(admin)/fields/page.tsx
@@ -25,7 +25,7 @@ export default async function FieldsPage() {
                 <Card className="w-full hover:shadow-lg hover:ring-1 ring-primary transition-shadow duration-300">
                   <CardContent className="flex flex-col justify-between">
                     <div className={ "flex items-center gap-4" }>
-                      <div className={ "overflow-hidden rounded-full p-3 bg-primary text-white aspect-square w-fit" }>
+                      <div className={ "overflow-hidden rounded-full p-3 bg-primary text-white aspect-square w-fit flex items-center justify-center" }>
                         <DynamicIcon name={ field.icon as IconName || 'circle-question-mark' } className="w-6 h-6"/>
                       </div>
                       <h3 className="text-md text-gray-900 mb-2">{ field.name }</h3>
