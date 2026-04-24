44079edd1c1348984e757059d7f25c18a5ac9fa2
Djafta <djafta@muendane.com>
2026-04-24 09:31:10 +0200

    Implement gallery page with category filters and lightbox features

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**/**',
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '1024mb',
